757121508ad5110e4fc32e3d98bd836aca58b58a
beasonshu <beasonshu@gmail.com>
2022-06-25 16:34:46 +0800

    趋势改为首页

--- extractor/src/test/java/org/schabi/newpipe/extractor/NewPipeTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/NewPipeTest.java
@@ -3,7 +3,6 @@ package org.schabi.newpipe.extractor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.schabi.newpipe.extractor.NewPipe.getServiceByUrl;
-import static org.schabi.newpipe.extractor.ServiceList.SoundCloud;
 import static org.schabi.newpipe.extractor.ServiceList.YouTube;
 
 import org.junit.jupiter.api.Test;
@@ -40,7 +39,5 @@ public class NewPipeTest {
         assertEquals(getServiceByUrl("https://www.youtube.com/playlist?list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH"), YouTube);
         assertEquals(getServiceByUrl("https://www.google.it/url?sa=t&rct=j&q=&esrc=s&cd=&cad=rja&uact=8&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DHu80uDzh8RY&source=video"), YouTube);
 
-        assertEquals(getServiceByUrl("https://soundcloud.com/pegboardnerds"), SoundCloud);
-        assertEquals(getServiceByUrl("https://www.google.com/url?sa=t&url=https%3A%2F%2Fsoundcloud.com%2Fciaoproduction&rct=j&q=&esrc=s&source=web&cd="), SoundCloud);
     }
 }
