51e8ca82fae4c170f5b53610d77a7a10e9512724
beasonshu <beasonshu@gmail.com>
2022-07-12 09:07:33 +0800

    add uc9 music channel extractor

--- extractor/src/main/java/org/schabi/newpipe/extractor/uc9muisc/U9MusicItemCollector.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/uc9muisc/U9MusicItemCollector.java
@@ -1,10 +1,14 @@
 package org.schabi.newpipe.extractor.uc9muisc;
 
 import org.schabi.newpipe.extractor.InfoItemsCollector;
+import org.schabi.newpipe.extractor.comments.CommentsInfoItem;
+import org.schabi.newpipe.extractor.comments.CommentsInfoItemExtractor;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class U9MusicItemCollector extends InfoItemsCollector<U9MusicItem, U9MusicExtractor> {
     public U9MusicItemCollector(int serviceId) {
@@ -13,7 +17,19 @@ public class U9MusicItemCollector extends InfoItemsCollector<U9MusicItem, U9Musi
 
     @Override
     public U9MusicItem extract(U9MusicExtractor extractor) throws ExtractionException, IOException {
-        extractor.getInitialPage();
-        return null;
+        U9MusicItem u9MusicItem = new U9MusicItem(getServiceId(), extractor.getUrl(), extractor.getName());
+
+        return u9MusicItem;
+    }
+
+
+
+    @Override
+    public void commit(final U9MusicExtractor extractor) {
+        try {
+            addItem(extract(extractor));
+        } catch (final Exception e) {
+            addError(e);
+        }
     }
 }
