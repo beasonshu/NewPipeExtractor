8aa357f4e6549e9e9f8d48dd2371cadb2a2a3d8d
shuxinghu <shuxinghu@shengqugames.com>
2022-07-15 10:20:18 +0800

    3个渠道使用默认国家和地区

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/YoutubeService.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/YoutubeService.java
@@ -174,8 +174,8 @@ public class YoutubeService extends StreamingService {
     @Override
     public KioskMusicList getKioskMusicList() throws ExtractionException{
         final KioskMusicList list = new KioskMusicList(this);
-        list.forceContentCountry(NewPipe.getPreferredContentCountry());
-        list.forceLocalization(NewPipe.getPreferredLocalization());
+//        list.forceContentCountry(NewPipe.getPreferredContentCountry());
+//        list.forceLocalization(NewPipe.getPreferredLocalization());
 
         // add kiosks here e.g.:
         try {
@@ -199,8 +199,8 @@ public class YoutubeService extends StreamingService {
     @Override
     public KioskGameList getKioskGameList() throws ExtractionException{
         final KioskGameList list = new KioskGameList(this);
-        list.forceContentCountry(NewPipe.getPreferredContentCountry());
-        list.forceLocalization(NewPipe.getPreferredLocalization());
+//        list.forceContentCountry(NewPipe.getPreferredContentCountry());
+//        list.forceLocalization(NewPipe.getPreferredLocalization());
 
         // add kiosks here e.g.:
         try {
@@ -224,8 +224,8 @@ public class YoutubeService extends StreamingService {
     @Override
     public KioskMovieList getKioskMovieList() throws ExtractionException{
         final KioskMovieList list = new KioskMovieList(this);
-        list.forceContentCountry(NewPipe.getPreferredContentCountry());
-        list.forceLocalization(NewPipe.getPreferredLocalization());
+//        list.forceContentCountry(NewPipe.getPreferredContentCountry());
+//        list.forceLocalization(NewPipe.getPreferredLocalization());
 
         // add kiosks here e.g.:
         try {
