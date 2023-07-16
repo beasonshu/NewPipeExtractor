78ac85db5149246dbbeb6c4cc40ef183790aed3d
AudricV <74829229+AudricV@users.noreply.github.com>
2023-07-16 20:40:52 +0200

    [PeerTube] Implement link handlers and channels tabs and tags changes on tests
    Co-authored-by: ThetaDev <t.testboy@gmail.com>

---
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/peertube/PeertubeChannelExtractorTest.java
@@ -0,0 +1,235 @@
+package org.schabi.newpipe.extractor.services.peertube;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.schabi.newpipe.downloader.DownloaderTestImpl;
+import org.schabi.newpipe.extractor.ExtractorAsserts;
+import org.schabi.newpipe.extractor.NewPipe;
+import org.schabi.newpipe.extractor.channel.tabs.ChannelTabs;
+import org.schabi.newpipe.extractor.exceptions.ParsingException;
+import org.schabi.newpipe.extractor.services.BaseChannelExtractorTest;
+import org.schabi.newpipe.extractor.services.peertube.extractors.PeertubeChannelExtractor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.schabi.newpipe.extractor.ExtractorAsserts.assertIsSecureUrl;
+import static org.schabi.newpipe.extractor.ExtractorAsserts.assertTabsContained;
+import static org.schabi.newpipe.extractor.ServiceList.PeerTube;
+
+/**
+ * Test for {@link PeertubeChannelExtractor}
+ */
+public class PeertubeChannelExtractorTest {
+
+    public static class LaQuadratureDuNet implements BaseChannelExtractorTest {
+        private static PeertubeChannelExtractor extractor;
+
+        @BeforeAll
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            // setting instance might break test when running in parallel
+            PeerTube.setInstance(new PeertubeInstance("https://framatube.org", "Framatube"));
+            extractor = (PeertubeChannelExtractor) PeerTube
+                    .getChannelExtractor("https://framatube.org/video-channels/lqdn_channel@video.lqdn.fr/videos");
+            extractor.fetchPage();
+        }
+
+        /*//////////////////////////////////////////////////////////////////////////
+        // Extractor
+        //////////////////////////////////////////////////////////////////////////*/
+
+        @Test
+        public void testServiceId() {
+            assertEquals(PeerTube.getServiceId(), extractor.getServiceId());
+        }
+
+        @Test
+        public void testName() throws ParsingException {
+            assertEquals("La Quadrature du Net", extractor.getName());
+        }
+
+        @Test
+        public void testId() throws ParsingException {
+            assertEquals("video-channels/lqdn_channel@video.lqdn.fr", extractor.getId());
+        }
+
+        @Test
+        public void testUrl() throws ParsingException {
+            assertEquals("https://framatube.org/video-channels/lqdn_channel@video.lqdn.fr", extractor.getUrl());
+        }
+
+        @Test
+        public void testOriginalUrl() throws ParsingException {
+            assertEquals("https://framatube.org/video-channels/lqdn_channel@video.lqdn.fr/videos", extractor.getOriginalUrl());
+        }
+
+        /*//////////////////////////////////////////////////////////////////////////
+        // ChannelExtractor
+        //////////////////////////////////////////////////////////////////////////*/
+
+        @Test
+        public void testDescription() throws ParsingException {
+            assertNotNull(extractor.getDescription());
+        }
+
+        @Test
+        public void testParentChannelName() throws ParsingException {
+            assertEquals("lqdn", extractor.getParentChannelName());
+        }
+
+        @Test
+        public void testParentChannelUrl() throws ParsingException {
+            assertEquals("https://video.lqdn.fr/accounts/lqdn", extractor.getParentChannelUrl());
+        }
+
+        @Test
+        public void testParentChannelAvatarUrl() throws ParsingException {
+            assertIsSecureUrl(extractor.getParentChannelAvatarUrl());
+        }
+
+        @Test
+        public void testAvatarUrl() throws ParsingException {
+            assertIsSecureUrl(extractor.getAvatarUrl());
+        }
+
+        @Test
+        public void testBannerUrl() throws ParsingException {
+            assertNull(extractor.getBannerUrl());
+        }
+
+        @Test
+        public void testFeedUrl() throws ParsingException {
+            assertEquals("https://framatube.org/feeds/videos.xml?videoChannelId=1126", extractor.getFeedUrl());
+        }
+
+        @Test
+        public void testSubscriberCount() throws ParsingException {
+            ExtractorAsserts.assertGreaterOrEqual(230, extractor.getSubscriberCount());
+        }
+
+        @Override
+        public void testVerified() throws Exception {
+            assertFalse(extractor.isVerified());
+        }
+
+        @Test
+        @Override
+        public void testTabs() throws Exception {
+            assertTabsContained(extractor.getTabs(), ChannelTabs.VIDEOS, ChannelTabs.PLAYLISTS);
+        }
+
+        @Test
+        @Override
+        public void testTags() throws Exception {
+            assertTrue(extractor.getTags().isEmpty());
+        }
+    }
+
+    public static class ChatSceptique implements BaseChannelExtractorTest {
+
+        private static PeertubeChannelExtractor extractor;
+
+        @BeforeAll
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            // setting instance might break test when running in parallel
+            PeerTube.setInstance(new PeertubeInstance("https://framatube.org", "Framatube"));
+            extractor = (PeertubeChannelExtractor) PeerTube
+                    .getChannelExtractor("https://framatube.org/api/v1/video-channels/chatsceptique@skeptikon.fr");
+            extractor.fetchPage();
+        }
+
+        /*//////////////////////////////////////////////////////////////////////////
+        // Extractor
+        //////////////////////////////////////////////////////////////////////////*/
+
+        @Test
+        public void testServiceId() {
+            assertEquals(PeerTube.getServiceId(), extractor.getServiceId());
+        }
+
+        @Test
+        public void testName() throws ParsingException {
+            assertEquals("Chat Sceptique", extractor.getName());
+        }
+
+        @Test
+        public void testId() throws ParsingException {
+            assertEquals("video-channels/chatsceptique@skeptikon.fr", extractor.getId());
+        }
+
+        @Test
+        public void testUrl() throws ParsingException {
+            assertEquals("https://framatube.org/video-channels/chatsceptique@skeptikon.fr", extractor.getUrl());
+        }
+
+        @Test
+        public void testOriginalUrl() throws ParsingException {
+            assertEquals("https://framatube.org/api/v1/video-channels/chatsceptique@skeptikon.fr", extractor.getOriginalUrl());
+        }
+
+        /*//////////////////////////////////////////////////////////////////////////
+        // ChannelExtractor
+        //////////////////////////////////////////////////////////////////////////*/
+
+        @Test
+        public void testDescription() throws ParsingException {
+            assertNotNull(extractor.getDescription());
+        }
+
+        @Test
+        public void testParentChannelName() throws ParsingException {
+            assertEquals("nathan", extractor.getParentChannelName());
+        }
+
+        @Test
+        public void testParentChannelUrl() throws ParsingException {
+            assertEquals("https://skeptikon.fr/accounts/nathan", extractor.getParentChannelUrl());
+        }
+
+        @Test
+        public void testParentChannelAvatarUrl() throws ParsingException {
+            assertIsSecureUrl(extractor.getParentChannelAvatarUrl());
+        }
+
+        @Test
+        public void testAvatarUrl() throws ParsingException {
+            assertIsSecureUrl(extractor.getAvatarUrl());
+        }
+
+        @Test
+        public void testBannerUrl() throws ParsingException {
+            assertNull(extractor.getBannerUrl());
+        }
+
+        @Test
+        public void testFeedUrl() throws ParsingException {
+            assertEquals("https://framatube.org/feeds/videos.xml?videoChannelId=137", extractor.getFeedUrl());
+        }
+
+        @Test
+        public void testSubscriberCount() throws ParsingException {
+            ExtractorAsserts.assertGreaterOrEqual(700, extractor.getSubscriberCount());
+        }
+
+        @Override
+        public void testVerified() throws Exception {
+            assertFalse(extractor.isVerified());
+        }
+
+        @Test
+        @Override
+        public void testTabs() throws Exception {
+            assertTabsContained(extractor.getTabs(), ChannelTabs.VIDEOS, ChannelTabs.PLAYLISTS);
+        }
+
+        @Test
+        @Override
+        public void testTags() throws Exception {
+            assertTrue(extractor.getTags().isEmpty());
+        }
+    }
+}
